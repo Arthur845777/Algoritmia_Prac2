45a7b64e5dc449da8b738d07ec0546438a15c586
SantiagoCusirramosChiri <santiago.cusirramos@ucsm.edu.pe>
2025-03-22 21:19:00 -0500

    En Reu con Miguel cambiamos el main de ConversorUnidadesGenericas

--- src/Fase1/P2/Ejercicio/ConversorUnidadesGenerico/Main.java
+++ src/Fase1/P2/Ejercicio/ConversorUnidadesGenerico/Main.java
@@ -1,4 +1,5 @@
 package Fase1.P2.Ejercicio.ConversorUnidadesGenerico;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -15,14 +16,14 @@ public class Main {
             do {
                 System.out.println("Elija una conversion");
                 System.out.println("1. Convertir longitud (metros, pies, pulgadas)\r\n" + //
-                                        "2. Convertir masa (kilogramos, libras, onzas).\r\n" + //
-                                        "3. Convertir temperatura (Celsius, Fahrenheit, Kelvin).\r\n" + //
-                                        "4. Convertir tiempo o (segundos, minutos, horas).\r\n" + //
-                                        "5. Salir del Programa.");
+                        "2. Convertir masa (kilogramos, libras, onzas).\r\n" + //
+                        "3. Convertir temperatura (Celsius, Fahrenheit, Kelvin).\r\n" + //
+                        "4. Convertir tiempo o (segundos, minutos, horas).\r\n" + //
+                        "5. Salir del Programa.");
                 opc=sc.nextInt();
                 sc.nextLine(); // Limpia el buffer antes de leer cadenas
             } while (opc<0 || opc>5);
-            
+
             do {
                 System.out.println("Ingrese la unidad inicial");
                 categoria=sc.nextLine();
@@ -30,55 +31,60 @@ public class Main {
                 System.out.println("Ingrese el valor de la unidad");
                 num1= sc.nextDouble();
                 uniG.setValor(num1);
-                sc.nextLine(); 
+                sc.nextLine();
                 System.out.println("Ingrese la unidad a la quese desea convertir");
                 cateC=sc.nextLine();
             } while (!UnidadesC(uniG,cateC));
-            
-            switch (opc) {
-                case 1:
-                    System.out.println("El resultado de convertir " + uniG.getTipo() + " a "+ cateC + "es\n"
-                    + Resultados("Longitud",uniG,cateC));
-                    break;
-            
-                case 2:
-                    System.out.println("El resultado de convertir " + uniG.getTipo() + " a "+ cateC + "es\n"
-                    + Resultados("Masa",uniG,cateC));  
-                    break;
-                case 3:
-                    System.out.println("El resultado de convertir " + uniG.getTipo() + " a "+ cateC + "es\n"
-                    + Resultados("Temperatura",uniG,cateC));          
-                    break;
-                            
-                case 4:
-                    System.out.println("El resultado de convertir " + uniG.getTipo() + " a "+ cateC + "es\n"
-                    + Resultados("Tiempo",uniG,cateC));        
-                    break;
-                default:
-                break;
 
+            boolean compo = Conversor.EsIgual(uniG, cateC);
+            if (!compo) {
+                switch (opc) {
+                    case 1:
+                        System.out.println("El resultado de convertir " + uniG.getTipo() + " a "+ cateC + "es\n"
+                                    + Resultados("Longitud",uniG,cateC));
+                        break;
+
+                    case 2:
+                        System.out.println("El resultado de convertir " + uniG.getTipo() + " a "+ cateC + "es\n"
+                                    + Resultados("Masa",uniG,cateC));
+                        break;
+
+                    case 3:
+                        System.out.println("El resultado de convertir " + uniG.getTipo() + " a "+ cateC + "es\n"
+                                    + Resultados("Temperatura",uniG,cateC));
+                        break;
+
+                    case 4:
+                        System.out.println("El resultado de convertir " + uniG.getTipo() + " a "+ cateC + "es\n"
+                                    + Resultados("Tiempo",uniG,cateC));
+                        break;
+
+                    default:
+                        break;
+                }
             }
+
         } while (opc!=5);
     }
-            
+
     public static <T extends Unidad<? extends Number>> boolean UnidadesC(T uni, String type) {
         ArrayList<String> Longitud = new ArrayList<>(Arrays.asList("metros", "pies", "pulgadas"));
         ArrayList<String> Masa = new ArrayList<>(Arrays.asList("kilogramos", "libras", "onzas"));
         ArrayList<String> Temperatura = new ArrayList<>(Arrays.asList("Celsius", "Fahrenheit", "Kelvin"));
         ArrayList<String> Tiempo = new ArrayList<>(Arrays.asList("segundos", "minutos", "horas"));
-    
+
         String tipoUnidad = uni.getTipo().trim().toLowerCase();
         String tipoChange = type.trim().toLowerCase();
-    
-        // Verificar si `tipoUnidad` y `type` pertenecen a la misma lista
+
+        // Verificar si tipoUnidad y type pertenecen a la misma lista
         if (Longitud.contains(tipoUnidad) && Longitud.contains(tipoChange)) return true;
         if (Masa.contains(tipoUnidad) && Masa.contains(tipoChange)) return true;
         if (Temperatura.contains(tipoUnidad) && Temperatura.contains(tipoChange)) return true;
         if (Tiempo.contains(tipoUnidad) && Tiempo.contains(tipoChange)) return true;
         System.out.println("Por favor ingrese unidades que en las que sea posible");
-        return false;  // Si no están en la misma lista, retorna `false`
+        return false;  // Si no están en la misma lista, retorna false
     }
-    
+
     public static <T extends Unidad<? extends Number>> double Resultados(String Cate,T uni, String type){
 
         if(Cate.equals("Longitud")){
@@ -94,5 +100,5 @@ public class Main {
         return 0;
     }
 
-    
+
 }
