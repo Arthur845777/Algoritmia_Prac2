a4977366abbf4bff588898ea75b8c1c1a95fbcf8
SantiagoCusirramosChiri <santiago.cusirramos@ucsm.edu.pe>
2025-06-23 16:28:31 -0500

    Arregla un poco la indentacion (Jose ha hecho el hash, santi esta dando indentacio) :v

--- src/Fase3/P11/HashingAbierto/Register.java
+++ src/Fase3/P11/HashingAbierto/Register.java
@@ -3,20 +3,25 @@ package Fase3.P11.HashingAbierto;
 public class Register{
 	private int key;
 	private String name;
+
 	public Register(int key, String name) {
 		super();
 		this.key = key;
 		this.name = name;
 	}
+
 	public int getKey() {
 		return key;
 	}
+
 	public void setKey(int key) {
 		this.key = key;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
--- src/Fase3/P11/HashingCerrado/HashC.java
+++ src/Fase3/P11/HashingCerrado/HashC.java
@@ -24,26 +24,45 @@ public class HashC {
 
     // Devuelve el primo más cercano
     public static int obtenerPrimoMasCercano(int n) {
-        if (n <= 2) return 2;
-        if (esPrimo(n)) return n;
+        if (n <= 2) {
+            return 2;
+        }
+        if (esPrimo(n)) {
+            return n;
+        }
 
         int menor = n;
         int mayor = n;
 
-        while (!esPrimo(menor)) menor--;
-        while (!esPrimo(mayor)) mayor++;
+        while (!esPrimo(menor)) {
+            menor--;
+        }
+
+        while (!esPrimo(mayor)) {
+            mayor++;
+        }
 
         return (n - menor) <= (mayor - n) ? menor : mayor;
     }
 
     // Verifica si un número es primo
     public static boolean esPrimo(int num) {
-        if (num < 2) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
+        if (num < 2) {
+            return false;
+        }
+
+        if (num == 2) {
+            return true;
+        }
+
+        if (num % 2 == 0) {
+            return false;
+        }
 
         for (int i = 3; i <= Math.sqrt(num); i += 2) {
-            if (num % i == 0) return false;
+            if (num % i == 0) {
+                return false;
+            }
         }
 
         return true;
@@ -61,12 +80,24 @@ public class HashC {
         do {
         	//Es nulo por borrado logico o esta disponible
             if (table[index] == null || table[index].isAvailable) {
-                if (table[index] == null) table[index] = new Element();
+
+                if (table[index] == null) {
+                    table[index] = new Element();
+                }
+
                 table[index].register = reg;
                 table[index].isAvailable = false; // ahora está ocupada
                 return;
             }
-            index = (index + 1) % size;
+
+            // validacion  extra
+            if (table[index].register.getKey() == key) {
+                System.out.println("Elemento ya existe");
+                return;
+            }
+
+            index = (index + 1) % size; // hash(index + 1)
+
         } while (index != originalIndex);
 
         System.out.println("Error: tabla hash llena.");
@@ -77,12 +108,19 @@ public class HashC {
         int originalIndex = index;
 
         do {
-            if (table[index] == null) return null;
+            if (table[index] == null) {
+                return null;
+            }
+
             //Si no esta disponible y es la clave correspondiente y no es nulo
-            if (!table[index].isAvailable && table[index].register.getKey() == key && table[index].register != null) {
+            if (!table[index].isAvailable && table[index].register.getKey() == key &&
+                    table[index].register != null) {
+
                 return table[index].register;
             }
+
             index = (index + 1) % size;
+
         } while (index != originalIndex);
 
         return null;
@@ -94,25 +132,33 @@ public class HashC {
 
         do {
             if (table[index] == null) return;
+
             //Si no esta disponible y es la clave correspondiente y no es nulo
-            if (!table[index].isAvailable && table[index].register.getKey() == key && table[index].register != null) {
+            if (!table[index].isAvailable && table[index].register.getKey() == key
+                    && table[index].register != null) {
+
                 table[index].register = null;
                 table[index].isAvailable = true; // ahora está disponible
                 return;
             }
+
             index = (index + 1) % size;
+
         } while (index != originalIndex);
     }
 
     public void printTable() {
         for (int i = 0; i < size; i++) {
+
             System.out.print("[" + i + "]: ");
-            //Si hay un registro y la tabla no esta disponible y el regitro no es nulo
+
             if (table[i] != null && !table[i].isAvailable && table[i].register != null) {
                 System.out.println(table[i].register);
+
             } else {
                 System.out.println("vacío");
             }
+
         }
     }
 }
