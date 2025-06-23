68362ec11b1a68a44be456e2cfa9374f33e97a2c
SantiagoCusirramosChiri <santiago.cusirramos@ucsm.edu.pe>
2025-06-23 18:24:32 -0500

    Hash abierto actualizado linkedList propia

--- src/Fase3/P11/HashingAbierto/HashO.java
+++ src/Fase3/P11/HashingAbierto/HashO.java
@@ -1,67 +1,134 @@
 package Fase3.P11.HashingAbierto;
 
-import java.util.LinkedList;
+import Fase3.P11.LinkedList.LinkedList;
 
 public class HashO {
     private LinkedList<Register>[] table;
     private int size;
 
     @SuppressWarnings("unchecked")
+
     public HashO(int size) {
-        this.size = size;
-        this.table = new LinkedList[size];
-        for (int i = 0; i < size; i++) {
+        this.size = obtenerPrimoMasCercano(size);
+        this.table = new LinkedList[this.size];
+
+        for (int i = 0; i < this.size; i++) {
             table[i] = new LinkedList<>();
         }
     }
 
+    public static int obtenerPrimoMasCercano(int n) {
+        if (n <= 2) {
+            return 2;
+        }
+        if (esPrimo(n)) {
+            return n;
+        }
+
+        int menor = n - 1;
+        int mayor = n + 1;
+
+        while (!esPrimo(menor)) {
+            menor--;
+        }
+
+        while (!esPrimo(mayor)) {
+            mayor++;
+        }
+
+        return (n - menor) <= (mayor - n) ? menor : mayor;
+    }
+
+
+    public static boolean esPrimo(int num) {
+        if (num < 2) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     private int hash(int key) {
         return key % size;
+
     }
 
     public void insert(Register reg) {
-        int index = hash(reg.getKey());
-
-        // Evitar duplicados
-        for (Register r : table[index]) {
-            if (r.getKey() == reg.getKey()) {
-                System.out.println("Clave duplicada: " + reg.getKey());
-                return;
-            }
+        if (reg == null) {
+            return;
         }
 
-        table[index].add(reg);
+        int index = hash(reg.getKey());
+        LinkedList<Register> list = table[index];
+
+        if (search(reg.getKey()) == null) {
+            return;
+        }
+
+        list.insertLast(reg);
     }
 
     public Register search(int key) {
         int index = hash(key);
+        LinkedList<Register> list = table[index];
 
-        for (Register r : table[index]) {
-            if (r.getKey() == key) {
-                return r;
+        for (int i = 0; i < list.length(); i++) {
+            Register record = list.getNodeAtIndex(i);
+
+            if (record != null && record.getKey() == key) {
+                return record;
             }
         }
 
         return null;
     }
 
-    public void delete(int key) {
+    public boolean delete(int key) {
         int index = hash(key);
+        LinkedList<Register> list = table[index];
 
-        table[index].removeIf(r -> r.getKey() == key);
+        for (int i = 0; i < list.length(); i++) {
+            Register record = list.getNodeAtIndex(i);
+
+            if (record != null && record.getKey() == key) {
+                return list.removeNode(record);
+            }
+
+        }
+
+        return false;
     }
 
     public void printTable() {
         for (int i = 0; i < size; i++) {
             System.out.print("[" + i + "]: ");
-            if (table[i].isEmpty()) {
+            LinkedList<Register> list = table[i];
+
+            if (list.isEmpty()) {
                 System.out.println("vacío");
             } else {
-                for (Register r : table[i]) {
+                for (int j = 0; j < list.length(); j++) {
+                    Register r = list.getNodeAtIndex(j);
                     System.out.print(r + " -> ");
                 }
                 System.out.println("null");
             }
         }
+    }
+
+
+    public int getSize() {
+        return size;
     }
 }
---
+++ src/Fase3/P11/LinkedList/LinkedList.java
@@ -0,0 +1,156 @@
+package Fase3.P11.LinkedList;
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private int countNodes;
+
+    public LinkedList() {
+        this.head = null;
+        countNodes = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int length() {
+        return countNodes;
+    }
+
+    public void destroyList() {
+        head = null;
+    }
+
+    public int findIndex(T nodo) {
+        if (head == null) {
+            return -1;
+        }
+
+        Node<T> NodeCurrent = head;
+        int cont = 0;
+
+        while (NodeCurrent != null) {
+            if (NodeCurrent.getData().equals(nodo)) {
+                return cont;
+            }
+            NodeCurrent = NodeCurrent.getNext();
+            cont++;
+        }
+        return -1;
+    }
+
+    public boolean search(T nodo) {
+        if (head == null) {
+            return false;
+        }
+
+        Node<T> NodeCurrent = head;
+
+        while (NodeCurrent != null) {
+            if (NodeCurrent.getData().equals(nodo)) {
+                return true;
+            }
+            NodeCurrent = NodeCurrent.getNext();
+        }
+        return false;
+    }
+
+    public T getNodeAtIndex(int index) {
+        if (index < 0 || index >= this.length()) {
+            return null;
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current.getData();
+    }
+
+    public boolean removeNode(T valor) {
+        if (head == null) {
+            return false;
+        }
+        if (head.getData().equals(valor)) {
+            head = head.getNext();
+            return true;
+        }
+
+        Node<T> NodeCurrent = head;
+
+        while (NodeCurrent.getNext() != null) {
+            if (NodeCurrent.getNext().getData().equals(valor)) {
+                NodeCurrent.setNext(NodeCurrent.getNext().getNext());
+                this.countNodes--;
+                return true;
+            }
+
+            NodeCurrent = NodeCurrent.getNext();
+        }
+
+        return false;
+    }
+
+    public void insertFirst(T valor) {
+        Node<T> newNode = new Node<T>(valor);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+        this.countNodes++;
+    }
+
+    public void insertLast(T valor) {
+        Node<T> newNode = new Node<T>(valor);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> NodeCurrent = head;
+            while (NodeCurrent.getNext() != null) {
+                NodeCurrent = NodeCurrent.getNext();
+            }
+            NodeCurrent.setNext(newNode);
+        }
+        this.countNodes++;
+    }
+
+    public void reverseLinkedList() {
+        if (head == null || head.getNext() == null) {
+            return;
+        }
+
+        Node<T> prevNode = null;
+        Node<T> currentNode = head;
+        Node<T> nextNode = null;
+
+        while (currentNode != null) {
+            nextNode = currentNode.getNext();
+            currentNode.setNext(prevNode);
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        head = prevNode;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<T> current = head;
+        while (current != null) {
+            sb.append(current.getData());
+            if (current.getNext() != null) {
+                sb.append(", ");
+            }
+            current = current.getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
